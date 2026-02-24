ac613b72b5d898889f5ff52eee6f71f85905acbd
Norbert Fila <nubet2137@gmail.com>
2026-02-24 23:18:06 +0100

    chore: add deployment configuration with GitHub pages

--- src/main.tsx
+++ src/main.tsx
@@ -7,7 +7,7 @@ import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/benford-law-visualizer">
       <AppProvider>
         <App />
       </AppProvider>
